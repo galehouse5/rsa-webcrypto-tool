aa0daef7b6def4d3cedd41214f8cbf819ae2ba99
galehouse5 <galehouse.5@gmail.com>
2018-11-11 15:54:22 -0500

    Fixed compatibility with FireFox.

--- scripts/encoding-helper.js
+++ scripts/encoding-helper.js
@@ -1,37 +1,46 @@
 var pemToBase64String = function (value, label) {
-    var lines = value.split("\n"),
-        base64String = ""; 
+    var lines = value.split("\n");
+    var base64String = "";
+
     for (var i = 0; i < lines.length; i++) {
         if (lines[i].startsWith("-----")) continue;
         base64String += lines[i];
     }
+
     return base64String;
 };
 
 var base64StringToArrayBuffer = function (value) {
-    var byteString = atob(value),
-        byteArray = new Uint8Array(byteString.length); 
+    var byteString = atob(value);
+    var byteArray = new Uint8Array(byteString.length); 
+
     for (var i = 0; i < byteString.length; i++) {
         byteArray[i] = byteString.charCodeAt(i);
     }
+
     return byteArray.buffer;
 };
 
 var base64StringToPem = function (value, label) {
     var pem = "-----BEGIN {0}-----\n".replace("{0}", label);
+
     for (var i = 0; i < value.length; i += 64) {
         pem += value.substr(i, 64) + "\n";
     }
+
     pem += "-----END {0}-----\n".replace("{0}", label);
+
     return pem;
 };
 
 var arrayBufferToBase64String = function (value) {
-    var byteArray = new Uint8Array(value),
-        byteString = "";
+    var byteArray = new Uint8Array(value);
+    var byteString = "";
+
     for (var i = 0; i < byteArray.byteLength; i++) {
         byteString += String.fromCharCode(byteArray[i]);
     }
+
     return btoa(byteString);
 };
 
